00a3a9559e4bb5db6954afb8518763edc2f385f1
Moisés <daw.moisesguerola@gmail.com>
2022-10-10 20:29:08 +0200

    GH-Pages

--- backend/module/contact/controller/ContactController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class ContactController {
-        function sendContactMessage() {
-            echo json_encode(Mailer::getInstance()->generateContactMail($_POST['name'], $_POST['email'], $_POST["text"], $_POST['theme']));
-        }
-    }
-
-?>
--- frontend/module/contact/controller/ContactController.js
+++
@@ -1,5 +0,0 @@
-app.controller('contactController', ($scope, contactServices) => {
-    $scope.sendContactMessage = () => {
-        contactServices.sendContactMessage($scope.name, $scope.email, $scope.theme, $scope.textMessage);
-    }
-});
